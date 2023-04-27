aca85032f7a3ffd053e3c83b6d8efea3e5a25e29
prashantt4333 <prashantt4333@gmail.com>
2023-04-27 02:39:27 -0700

    okk

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 
 
